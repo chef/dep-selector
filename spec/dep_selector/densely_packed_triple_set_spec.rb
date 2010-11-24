948db66ef8a8dc3430a14ed49c3d3121fd2daad9
Mark Anderson <ot-git@muking.org>
2010-11-23 17:01:46 -0800

    Complete set of tests.

--- spec/dep_selector/densely_packed_triple_set_spec.rb
+++ spec/dep_selector/densely_packed_triple_set_spec.rb
@@ -22,14 +22,52 @@ describe DepSelector::DenselyPackedTripleSet do
     range.last.should == 1
   end
 
-  it "can create a simple set of versions and pick a version by greater than" do
+  it "can create a simple set of versions and pick a version by greater than equal" do
     dpt_set = DepSelector::DenselyPackedTripleSet.new ["1.0.0", "2.0.0", "3.0.0", "4.0.0"]
     constraint = DepSelector::VersionConstraint.new(">= 2.0.0")
     range = dpt_set[constraint]
     range.first.should == 1
     range.last.should == 3
   end
+  it "can create a simple set of versions and pick a version by greater than" do
+    dpt_set = DepSelector::DenselyPackedTripleSet.new ["1.0.0", "2.0.0", "3.0.0", "4.0.0"]
+    constraint = DepSelector::VersionConstraint.new("> 2.0.0")
+    range = dpt_set[constraint]
+    range.first.should == 2
+    range.last.should == 3
+  end
+  
+  it "can create a simple set of versions and pick a version by less than equal" do
+    dpt_set = DepSelector::DenselyPackedTripleSet.new ["1.0.0", "2.0.0", "3.0.0", "4.0.0"]
+    constraint = DepSelector::VersionConstraint.new("<= 3.0.0")
+    range = dpt_set[constraint]
+    range.first.should == 0
+    range.last.should == 2
+  end
 
+  it "can create a simple set of versions and pick a version by less than equal" do
+    dpt_set = DepSelector::DenselyPackedTripleSet.new ["1.0.0", "2.0.0", "3.0.0", "4.0.0"]
+    constraint = DepSelector::VersionConstraint.new("<= 3.0.0")
+    range = dpt_set[constraint]
+    range.first.should == 0
+    range.last.should == 2
+  end
 
+  it "can create a more complex set of versions and pick a version by >~ x.y" do
+    dpt_set = DepSelector::DenselyPackedTripleSet.new ["0.1.0", "1.0.0", "1.1", "1.1.1", "1.2", "1.2.1", "1.2.2",
+                                                       "2.0.0", "3.0.0", "4.0.0"]
+    constraint = DepSelector::VersionConstraint.new("~> 1.1")
+    range = dpt_set[constraint]
+    range.first.should == 2
+    range.last.should == 6
+  end
+  it "can create a more complex set of versions and pick a version by ~> x.y.z" do
+    dpt_set = DepSelector::DenselyPackedTripleSet.new ["0.1.0", "1.0.0", "1.1", "1.1.1", "1.2", "1.2.1", "1.2.2",
+                                                       "2.0.0", "3.0.0", "4.0.0"]
+    constraint = DepSelector::VersionConstraint.new("~> 1.1.0")
+    range = dpt_set[constraint]
+    range.first.should == 2
+    range.last.should == 3
+  end
 
 end
