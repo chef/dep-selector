e6f45ef1767514b00687f8e7efc3e4ebd4422fe9
Chris Walters <cw@opscode.com>
2010-11-28 21:44:47 -0800

    Removing dead code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,11 +9,6 @@ def setup_constraint(dep_graph, cset)
       pv.dependencies << DepSelector::Dependency.new(dep_graph.package(dep_name), constraint)
     end
   end
-  # commenting out, because Selector relies on DependencyGraph's clone
-  # method, which dies if the constraints are already generated when
-  # it's asked to solve. This is a temporary condition until the
-  # Selector interface is solidified and the tests migrated.
-#  dep_graph.generate_gecode_constraints
 end
 
 def add_run_list(dep_graph, run_list)
@@ -26,72 +21,5 @@ def add_run_list(dep_graph, run_list)
       pkg_mv.must_be.in(pkg.densely_packed_versions[constraint])
     end
     dep_graph.branch_on(pkg_mv)
-  end
-end
-
-def init_objective_function(dep_graph, run_list, current_versions)
-  current_versions_densely_packed = current_versions.inject({}) do |acc, elt|
-    dpv = dep_graph.package(elt.first).densely_packed_versions
-    version_spec = DepSelector::VersionConstraint.new("= #{elt.last}")
-#    pp :dpv=>dpv, :elt_first=>elt.first, :version_spec=>version_spec
-    acc[elt.first] = dep_graph.package(elt.first).densely_packed_versions[version_spec].first
-    acc
-  end
-  
-
-# in our objective function
-  explicit_densely_packed_dependencies = run_list.inject({}) do |acc, rli| 
-     version_spec = DepSelector::VersionConstraint.new(rli.last)
-    acc[rli.first] = dep_graph.package(rli.first).densely_packed_versions[version_spec] ;
-    acc 
-  end
-
-  objective_function = DepSelector::ObjectiveFunction.new do |soln|
-    # Note: We probably have to filter out the unnecessary
-    # dependencies that are nonetheless bound here so that we're not
-    # unjustly punishing the solution under consideration for
-    # appearing to change packages that will actually just get
-    # removed.
-    edit_distance = current_versions.inject(0) do |acc, curr_version|
-      # TODO [cw,2010/11/21]: This edit distance only increases when a
-      # package that is currently deployed is changed, not when a new
-      # dependency is added. I think there is an argument to be made
-      # that also including new packages is worthy of an edit distance
-      # bump, since the interpretation can be that any difference in
-      # code that is run (not just changing existing code) could be
-      # considered "infrastructure instability". This needs to be
-      # considered.
-      pkg_name, curr_version = curr_version
-      if soln.has_key?(pkg_name)
-        putative_version = soln[pkg_name]
-        puts "#{pkg_name} going from #{curr_version} to #{putative_version}"
-        acc -= 1 unless putative_version == curr_version
-      end
-      acc
-    end
-  end
-end
-
-def dump_result(dep_graph, objective_function)
-  puts "Results"
-  objective_function.best_solution.keys.sort.each do |pkg_name|
-    densely_packed_version = objective_function.best_solution[pkg_name]
-    if !(densely_packed_version.nil?)
-      puts "#{pkg_name}: #{densely_packed_version} -> #{dep_graph.package(pkg_name).version_from_densely_packed_version(densely_packed_version)}"
-    end
-  end
-end
-
-def verify_result(dep_graph, objective_function, expected_values)
-  expected_values.each_pair do |pkg_name, version|
-    densely_packed_version = objective_function.best_solution[pkg_name]
-    pp :densely_packed_version => densely_packed_version
-    densely_packed_version.should_not be_nil
-    if !(densely_packed_version.nil?) 
-      computed_version = dep_graph.package(pkg_name).version_from_densely_packed_version(densely_packed_version).to_s
-      computed_version.should == version
-    else
-
-    end
   end
 end
