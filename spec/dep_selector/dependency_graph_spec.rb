49bff4a50603e719001a0adf2e6bd58aee89e287
Mark Anderson <ot-git@muking.org>
2010-11-28 16:14:24 -0800

    Clean up more cruft.

--- spec/dep_selector/dependency_graph_spec.rb
+++ spec/dep_selector/dependency_graph_spec.rb
@@ -2,29 +2,6 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..','spec_helper'))
 
 require 'pp'
 
-simple_cookbook_version_constraint =
-  [{"key"=>["A", "1.0.0"], "value"=>{"B"=>"= 2.0.0"}},
-   {"key"=>["A", "2.0.0"], "value"=>{"B"=>"= 1.0.0", "C"=>"= 1.0.0"}},
-   {"key"=>["B", "1.0.0"], "value"=>{}},
-   {"key"=>["B", "2.0.0"], "value"=>{}},
-   {"key"=>["C", "1.0.0"], "value"=>{}}]
-
-
-complex_cookbook_version_constraint =
-  [{"key"=>["A", "1.0.0"], "value"=>{"B"=>"= 2.0.0"}},
-   {"key"=>["A", "2.0.0"], "value"=>{"B"=>"= 1.0.0", "C"=>"= 1.0.0"}},
-   {"key"=>["B", "1.0.0"], "value"=>{}},
-   {"key"=>["B", "2.0.0"], "value"=>{}},
-   {"key"=>["C", "1.0.0"], "value"=>{"D"=>">= 1.0.0"}},
-   {"key"=>["C", "2.0.0"], "value"=>{"D"=>">= 2.0.0"}},
-   {"key"=>["C", "3.0.0"], "value"=>{"D"=>">= 3.0.0"}},
-   {"key"=>["C", "4.0.0"], "value"=>{"D"=>">= 4.0.0"}},
-   {"key"=>["D", "1.0.0"], "value"=>{}},
-   {"key"=>["D", "2.0.0"], "value"=>{}},
-   {"key"=>["D", "3.0.0"], "value"=>{}},
-   {"key"=>["D", "4.0.0"], "value"=>{}},
-]
-
 describe DepSelector::DependencyGraph do
   it "can create a package named foo" do
     dep_graph = DepSelector::DependencyGraph.new
