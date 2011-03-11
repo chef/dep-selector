f48358aa2215b5929ec5ade2fef399ab4ce74c8d
Mark Anderson <ot-git@muking.org>
2011-03-11 14:34:03 -0800

    Add protective checks to the gecode_wrapper

--- lib/dep_selector/gecode_wrapper.rb
+++ lib/dep_selector/gecode_wrapper.rb
@@ -39,17 +39,29 @@ module DepSelector
     def self.finalize(gecode_problem)
       proc { Dep_gecode.VersionProblemDestroy(gecode_problem) }
     end
+
+    def check_package_id(package_id, param_name)
+      raise "Gecode #{param_name} is out of range #{package_id}" unless (package_id >= 0 && package_id < self.size()) 
+    end
     
     def size()
+      raise "Gecode internal failure" if gecode_problem.nil?
       Dep_gecode.VersionProblemSize(gecode_problem)
     end
     def package_count()
+      raise "Gecode internal failure" if gecode_problem.nil?
       Dep_gecode.VersionProblemPackageCount(gecode_problem)
     end
     def add_package(min, max, current_version)
+      raise "Gecode internal failure" if gecode_problem.nil?
       Dep_gecode.AddPackage(gecode_problem, min, max, current_version)
     end
+
     def add_version_constraint(package_id, version, dependent_package_id, min_dependent_version, max_dependent_version)
+      raise "Gecode internal failure" if gecode_problem.nil?
+      check_package_id(package_id, "package_id")
+      check_package_id(dependent_package_id, "dependent_package_id")
+
       # Valid package versions are between -1 and its max (-1 means
       # don't care, meaning it doesn't need to be assigned). To
       # indicate constraints that match no versions, -2 is used, since
@@ -66,6 +78,7 @@ module DepSelector
       # that portion of the solution space unsatisfiable. Thus it is
       # impossible to find solutions dependent on non-existent
       # packages.
+
       min = min_dependent_version || NoMatchConstraint
       max = max_dependent_version || NoMatchConstraint
       Dep_gecode.AddVersionConstraint(gecode_problem, package_id, version, dependent_package_id, min, max)
@@ -78,22 +91,33 @@ module DepSelector
       end
     end
     def get_package_version(package_id)
+      raise "Gecode internal failure" if gecode_problem.nil?
+      check_package_id(package_id, "package_id")
       Dep_gecode.GetPackageVersion(gecode_problem, package_id)
     end
     def is_package_disabled?(package_id)
+      raise "Gecode internal failure" if gecode_problem.nil?
+      check_package_id(package_id, "package_id")
       Dep_gecode.GetPackageDisabledState(gecode_problem, package_id);
     end
     
     def get_package_max(package_id) 
+      raise "Gecode internal failure" if gecode_problem.nil?
+      check_package_id(package_id, "package_id")
       Dep_gecode.GetPackageMax(gecode_problem, package_id)
     end
     def get_package_min(package_id) 
+      raise "Gecode internal failure" if gecode_problem.nil?
+      check_package_id(package_id, "package_id")
       Dep_gecode.GetPackageMin(gecode_problem, package_id)
     end
     def dump() 
+      raise "Gecode internal failure" if gecode_problem.nil?
       Dep_gecode.VersionProblemDump(gecode_problem)
     end
     def dump_package_var(package_id)
+      raise "Gecode internal failure" if gecode_problem.nil?
+      check_package_id(package_id, "package_id")
       Dep_gecode.VersionProblemPrintPackageVar(gecode_problem, package_id)
     end
 
@@ -104,17 +128,21 @@ module DepSelector
 
     def mark_required(package_id)
       raise "Gecode internal failure (mark_required)" if gecode_problem.nil?
+      check_package_id(package_id, "package_id")
       Dep_gecode.MarkPackageRequired(gecode_problem, package_id);
     end
 
     def mark_preferred_to_be_at_latest(package_id, weight)
-      raise "Gecode internal failure (mark_preferred)" if gecode_problem.nil?
+      raise "Gecode internal failure (mark_preferred_to_be_at_latest)" if gecode_problem.nil?
+      check_package_id(package_id, "package_id")
       Dep_gecode.MarkPackagePreferredToBeAtLatest(gecode_problem, package_id, weight);
     end
 
     def solve()
+      raise "Gecode internal failure (solve)" if gecode_problem.nil?
       solution = GecodeWrapper.new(Dep_gecode.Solve(gecode_problem))
       raise "Gecode internal failure (no solution found)" if (solution.nil?)
+
       raise Exceptions::NoSolutionFound.new(solution) if solution.package_disabled_count > 0
       solution
     end
