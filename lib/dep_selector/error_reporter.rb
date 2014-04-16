446aa0ec95712e40579df97984e356944a77543e
Lamont Granquist <lamont@scriptkiddie.org>
2014-04-16 10:43:32 -0700

    change signature of DI API

--- lib/dep_selector/error_reporter.rb
+++ lib/dep_selector/error_reporter.rb
@@ -20,8 +20,8 @@
 module DepSelector
   class ErrorReporter
 
-    def give_feedback(workspace, solution_constraints, unsatisfiable_constraint_idx, most_constrained_package)
-      raise "Sub-class must implement"
+    def handle_errors(workspace, solution_constraints, idx, dep_graph, valid_packages, packages_to_include_in_solve, nsf)
+      raise "Sub-class must implement handle_errors, the dependency injection API has changed"
     end
 
   end
